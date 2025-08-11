3d0bd1b5c4279a366ba4061a9dde8806a18d123c
UZB-POLO <axmedovulfat689@gmail.com>
2025-08-11 07:36:28 +0500

    Some changes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3000);
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running on 3000');
 }
 bootstrap();
